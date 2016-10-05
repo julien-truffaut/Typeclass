0b02e19c1a4b3791b092a9d8d9bbc99a25ced7d2
Julien Truffaut <truffaut.julien@gmail.com>
2016-10-05 22:12:09 +0100

    add slide about newtype

--- exercice/src/main/scala/typeclass/data/Mult.scala
+++ exercice/src/main/scala/typeclass/data/Mult.scala
@@ -10,6 +10,6 @@ object Mult {
   implicit val gen: Gen[Mult] = Gen[Int].map(Mult(_))
 
   implicit val semigroup: Semigroup[Mult] = new Semigroup[Mult] {
-    def combine(x: Mult, y: Mult): Mult = ???
+    def combine(x: Mult, y: Mult): Mult = Mult(x.value * y.value)
   }
 }

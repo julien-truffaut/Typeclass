6a75fb079f6c7712907fb45a8e237fca68abab8e
Julien Truffaut <truffaut.julien@gmail.com>
2016-10-08 14:16:07 +0100

    add validation slide

--- exercice/src/main/scala/typeclass/data/Validation.scala
+++ exercice/src/main/scala/typeclass/data/Validation.scala
@@ -1,6 +1,6 @@
 package typeclass.data
 
-import typeclass.{Applicative, Semigroup}
+import typeclass.Functor
 
 import scalaprops.Gen
 
@@ -20,15 +20,8 @@ object Validation {
   def failure[E, A](e: E): Validation[E, A] = Failure(e)
   def success[E, A](a: A): Validation[E, A] = Success(a)
 
-  implicit def validationApplicative[E](implicit E: Semigroup[E]): Applicative[Validation[E, ?]] = new Applicative[Validation[E, ?]] {
-    def pure[A](a: A): Validation[E, A] = success(a)
-    def ap[A, B](fab: Validation[E, A => B], fa: Validation[E, A]): Validation[E, B] =
-      (fab, fa) match {
-        case (Success(f), Success(a)) => Success(f(a))
-        case (Failure(e), Success(_)) => Failure(e)
-        case (Success(_), Failure(e)) => Failure(e)
-        case (Failure(x), Failure(y)) => Failure(E.combine(x, y))
-      }
+  implicit def validationFunctor[E]: Functor[Validation[E, ?]] = new Functor[Validation[E, ?]] {
+    def map[A, B](fa: Validation[E, A])(f: (A) => B): Validation[E, B] = ???
   }
 
   implicit def gen[E: Gen, A: Gen]: Gen[Validation[E, A]] =

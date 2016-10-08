6aaad6afb8e4dfa7bdf509cfb8ab249e277bb44e
Julien Truffaut <truffaut.julien@gmail.com>
2016-10-08 14:07:20 +0100

    add slides about implicits

---
+++ answer/src/main/scala/typeclass/std/Long.scala
@@ -0,0 +1,12 @@
+package typeclass.std
+
+import typeclass.Monoid
+
+object long {
+
+  implicit val longMonoid: Monoid[Long] = new Monoid[Long] {
+    def empty: Long = 0
+    def combine(x: Long, y: Long): Long = x + y
+  }
+
+}
